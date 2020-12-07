93acfe816fe22c33afcff7cf6e056d8c8902c0fe
AlineNecchi <alinenecchi@gmail.com>
2020-12-06 23:23:39 -0300

    @alandaleote Add menu

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
+import PySimpleGUI as gui
+from insertClass import insertClass
+
+
+
+class FormMenu:
+    def __init__(self):
+        conteudo = [
+            [gui.Text(menu)],
+            [gui.Input()],
+            [gui.Button("Abrir")]
+        ]
+        self.tela = gui.Window("English School").layout(conteudo)
+
+    def show(self):
+        self.button, self.valores = self.tela.Read()
+        escolha = self.valores[0]
+        if escolha == '1':
+            insertClass()
+        elif escolha == '2':
+            pass
+        elif escolha == '3':
+            pass
+        elif escolha == '4':
+            pass
+        else:
+            input('Opção inválida! [Enter]')
+       
+
+
+
+
+
 
 menu = '''
-+-------------------------------------+
-|              Menu                   |
-+-------------------------------------+
-|  1 -              |
-|  2 -               |
-|  3 -            |
-|  4 -           |
-|  5 -      |
-|  0 -                    |
-+-------=-----------------------------+
-Escolha: '''
 
+1 - Inserir Turma                  
+2 - Inserir Professor              
+3 - Inserir Aluno                  
+4 - Listar Alunos                  
 
-escolha = 0
-while True:
-    escolha = input(menu)
-    if escolha == '0':
-        break
-    elif escolha == '1':
-       pass
-    elif escolha == '2':
-       pass
-    elif escolha == '3':
-       pass
-    else:
-        input('Opção inválida! [Enter]')
+Insira a sua escolha:
+ '''
+
+form = FormMenu()
+newclass = form.show()
+    
+    
+    
