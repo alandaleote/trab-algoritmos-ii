bf006422ca0abb0c8da8ee5894f2ba6ec450b95c
alandaleote <alandaleote@gmail.com>
2020-12-06 22:01:34 -0300

    @alinenecchi update classes

--- Student.py
+++ Student.py
@@ -1,7 +1,7 @@
 from Person import Person 
 
 class Student(Person):
-    def __init__(self,name, country, module, weekday):
+    def __init__(self,name=None, country=None, module=None, weekday=None):
         Person.__init__(self, name, country)
         self.id = None
         self.module = module
