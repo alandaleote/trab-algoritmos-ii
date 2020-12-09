f29848b4b3896cba950fc5dd5902b8a2ad280da1
AlineNecchi <alinenecchi@gmail.com>
2020-12-08 22:04:22 -0300

    @alandaleote add deleteStudents

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import PySimpleGUI as gui
+
+from delete import deleteStudent
 from insertClass import insertClass
+from insertStudent import insertStudente
+from insertTeacher import insertTeacher
+from listStudents import listEstudents
+
 gui.theme('Dark Brown')
+
 class FormMenu:
     def __init__(self):
         conteudo = [
@@ -9,34 +16,28 @@ class FormMenu:
             [gui.Button("Abrir")]
         ]
         self.tela = gui.Window("English School").layout(conteudo)
-
     def show(self):
         self.button, self.valores = self.tela.Read()
         escolha = self.valores[0]
         if escolha == '1':
             insertClass()
         elif escolha == '2':
-            pass
+            insertTeacher()
         elif escolha == '3':
-            pass
+            insertStudente()
         elif escolha == '4':
-            pass
+            listEstudents()
+        elif escolha == '5':
+            deleteStudent()
         else:
-            input('Opção inválida! [Enter]')
-            "arquivo"
-       
-
-
-
-
-
-
+            input('OpÃ§Ã£o invÃ¡lida! [Enter]')
 menu = '''
 
 1 - Inserir Turma                  
 2 - Inserir Professor              
 3 - Inserir Aluno                  
-4 - Listar Alunos                  
+4 - Listar Alunos
+5 - Deletar Estudantes                 
 
 Insira a sua escolha:
  '''
