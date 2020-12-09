6e5cd4ed1c6c48d9bb7f2e330c83e42b8eda201a
alandaleote <alandaleote@gmail.com>
2020-12-08 21:58:33 -0300

    @alinenecchi add  lists methods

--- listClasses.py
+++ listClasses.py
@@ -1,15 +1,15 @@
 import mysql.connector
 
-def listStudents():
+def listClasses():
     conn = mysql.connector.connect(host='localhost', database='english_school', user='root', password='')
     if conn.is_connected():
         cursor = conn.cursor()
 
-        cursor.execute("SELECT s.name, s.country, s.module, s.weekday FROM students s order by s.module")
+        cursor.execute("SELECT c.module, c.weekday, c.teachername FROM classes c order by c.weekday")
         result = cursor.fetchall()
 
         for data in result:
-            print("Nome: {:>20} País: {:>20} Módulo: {:>15} - {:>15} ".format(data[0], data[1], data[2], data[3]))
+            print("Módulo: {:>15} - {:>15} Professor {:>20}".format(data[0], data[1], data[2]))
 
         cursor.close()
         conn.close()
--- listTeachers.py
+++ listTeachers.py
@@ -1,15 +1,15 @@
 import mysql.connector
 
-def listStudents():
+def listTeachers():
     conn = mysql.connector.connect(host='localhost', database='english_school', user='root', password='')
     if conn.is_connected():
         cursor = conn.cursor()
 
-        cursor.execute("SELECT s.name, s.country, s.module, s.weekday FROM students s order by s.module")
+        cursor.execute("SELECT t.name, t.country, t.module, t.weekday FROM teachers t order by t.name")
         result = cursor.fetchall()
 
         for data in result:
-            print("Nome: {:>20} País: {:>20} Módulo: {:>15} - {:>15} ".format(data[0], data[1], data[2], data[3]))
+            print("Nome: {:>20} País: {:>20} Turma: {:>15} - {:>15} ".format(data[0], data[1], data[2], data[3]))
 
         cursor.close()
         conn.close()
