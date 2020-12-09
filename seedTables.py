c6bc33d187d0cf2b405020046edb1e7827303dc8
alandaleote <alandaleote@gmail.com>
2020-12-09 20:39:13 -0300

    update seed

--- seedTables.py
+++ seedTables.py
@@ -4,9 +4,9 @@ import mysql.connector
 conn = mysql.connector.connect(host='localhost', database='english_school', user='root', password='')
 
 
-module = ('Iniciante', 'Basico', 'Intermediario', 'Avancado')
-weekday = ('Segunda-feira', 'Terca-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabado', 'Domingo')
-country = ('Brasil', 'Canada', 'France', 'Germany', 'England', 'Argentina', 'Spain', 'Italy', 'Netherlands', 'US' )
+module = ['Iniciante', 'Basico', 'Intermediario', 'Avancado']
+weekday = ['Domingo', 'Segunda-feira', 'Terca-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabado']
+country = ['Brasil', 'Canada', 'France', 'Germany', 'England', 'Argentina', 'Spain', 'Italy', 'Netherlands', 'US' ]
 
 
 # testando a conexão com Banco de Dados
@@ -15,41 +15,42 @@ if conn.is_connected():
    
     cursor = conn.cursor()
 
-    query = "INSERT INTO STUDENTS (name, country, module, weekday) "
-    query += "values ('Alanda Leote', 'Canada', 'Avancado', 'Sabado'), "
-    query += "values ('Aline Necchi', 'Brasil', 'Intermediario', 'Domingo'), "
-    query += "values ('Bernardo Araujo', 'Canada', 'Avancado', 'Sabado'), "
-    query += "values ('Vanessa Thompson', 'France', 'Intermediario', 'Quinta-feira'), "
-    query += "values ('Bianca Whitaker', 'Germany', 'Avancado', 'Segunda-feira'), "
-    query += "values ('Margarete Goodwin', 'France', 'Avancado', 'Segunda-feira'), "
-    query += "values ('Derick Mclaughlin', 'Brasil', 'Intermediario', 'Quinta-feira'), "
-    query += "values ('Carina Richardson', 'Spain', 'Basico', 'Quarta-feira'), "
-    query += "values ('Oswaldo Hays', 'Germany', 'Iniciante', 'Terca-feira'), "
-    query += "values ('Johan Snow', 'England', 'Intermediario', 'Domingo'), "
-    query += "values ('Noel Green', 'England', 'Iniciante', 'Terca-feira'), "
-    query += "values ('Selena Benton', 'Argentina', 'Intermediario', 'Domingo'), "
-    query += "values ('David Phillips', 'Netherlands', 'Intermediario', 'Domingo'), "
-    query += "values ('Jack Brooks', 'Italy', 'Intermediario', 'Quinta-feira'); "
+    query = " "
+    query += "values ('Alanda Leote', '{}', '{}', '{}'), ".format(country[1], module[3], weekday[6])
+    query += "values ('Aline Necchi', '{}', '{}', '{}'), ".format(country[0], module[2], weekday[0])
+    query += "values ('Bernardo Araujo', '{}', '{}', '{}'), ".format(country[1], module[3], weekday[6])
+    query += "values ('Vanessa Thompson', '{}', '{}', '{}'), ".format(country[2], module[2], weekday[4])
+    query += "values ('Bianca Whitaker', '{}', '{}', '{}'), ".format(country[3], module[3], weekday[1])
+    query += "values ('Margarete Goodwin', '{}', '{}', '{}'), ".format(country[2], module[3], weekday[1])
+    query += "values ('Derick Mclaughlin', '{}', '{}', '{}'), ".format(country[0], module[2], weekday[4])
+    query += "values ('Carina Richardson', '{}', '{}', '{}'), ".format(country[1], module[1], weekday[3])
+    query += "values ('Oswaldo Hays', '{}', '{}', '{}'), ".format(country[1], module[0], weekday[2])
+    query += "values ('Johan Snow', '{}', '{}', '{}'), ".format(country[4], module[2], weekday[0])
+    query += "values ('Noel Green', '{}', '{}', '{}'), ".format(country[4], module[0], weekday[2])
+    query += "values ('Selena Benton', '{}', '{}', '{}'), ".format(country[5], module[2], weekday[0])
+    query += "values ('David Phillips', '{}', '{}', '{}'), ".format(country[8], module[2], weekday[0])
+    query += "values ('Jack Brooks', '{}', '{}', '{}'); ".format(country[7], module[2], weekday[4])
     cursor.execute(query)
 
-    query = "INSERT INTO TEACHERS (name, country, module, weekday)"
-    query += " values ('Sophia Goodwin', 'England', 'Iniciante', 'Terca-feira'),"
-    query += " values ('Alfred Lambert', 'England', 'Basico', 'Quarta-feira'),"
-    query += " values ('Derick Riggs', 'US', 'Intermediario', 'Domingo'),"
-    query += " values ('Derick Riggs', 'US', 'Intermediario', 'Quinta-feira'),"
-    query += " values ('Trevor Norman', 'Canada', 'Avancado', 'Sabado'),"
-    query += " values ('Meaghan Bender', 'Canada', 'Avancado', 'Segunda-feira');"
+    query = "INSERT INTO teachers(name, country, module, weekday)"
+    query += " values ('Sophia Goodwin', '{}', '{}', '{}'),".format(country[4], module[0], weekday[2])
+    query += " values ('Alfred Lambert', '{}', '{}', '{}'),".format(country[4], module[1], weekday[3])
+    query += " values ('Derick Riggs', '{}', '{}', '{}'),".format(country[9], module[2], weekday[0])
+    query += " values ('Derick Riggs', '{}', '{}', '{}'),".format(country[9], module[2], weekday[4])
+    query += " values ('Trevor Norman', '{}', '{}', '{}'),".format(country[1], module[3], weekday[6])
+    query += " values ('Meaghan Bender', '{}', '{}', '{}');".format(country[1], module[3], weekday[1])
     cursor.execute(query)
 
-    query = "INSERT INTO CLASSES (module, weekday, teachername)"
-    query += " values ('Iniciante', 'Terca-feira', 'Sophia Goodwin'),"
-    query += " values ('Basico', 'Quarta-feira', 'Alfred Lambert'),"
-    query += " values ('Intermediario', 'Domingo', 'Derick Riggs'),"
-    query += " values ('Intermediario', 'Quinta-feira', 'Derick Riggs'),"
-    query += " values ('Avancado', 'Sabado', 'Trevor Norman'),"
-    query += " values ('Avancado', 'Segunda-feira', 'Meaghan Bender');"
+    query = "INSERT INTO classes(module, weekday, teachername)"
+    query += " values ('{}', '{}', 'Sophia Goodwin'),".format(module[0], weekday[2])
+    query += " values ('{}', '{}', 'Alfred Lambert'),".format(module[1], weekday[3])
+    query += " values ('{}', '{}', 'Derick Riggs'),".format(module[2], weekday[0])
+    query += " values ('{}', '{}', 'Derick Riggs'),".format(module[2], weekday[4])
+    query += " values ('{}', '{}', 'Trevor Norman'),".format(module[3], weekday[6])
+    query += " values ('{}', '{}', 'Meaghan Bender');".format(module[3], weekday[1])
     cursor.execute(query)
 
+    conn.commit()
     cursor.close()
     conn.close()
 else:
